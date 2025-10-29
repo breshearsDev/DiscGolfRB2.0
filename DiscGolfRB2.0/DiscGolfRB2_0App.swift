881df44ca19fde4acec4fc290fb87e0ffd39a2d7
Rob Breshears <robbreshears@gmail.com>
2025-10-28 18:42:15 -0600

    Initial Commit

---
+++ DiscGolfRB2.0/DiscGolfRB2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  DiscGolfRB2_0App.swift
+//  DiscGolfRB2.0
+//
+//  Created by Breshears, Rob - CTC on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscGolfRB2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiscGolfRB2.0Tests/DiscGolfRB2_0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DiscGolfRB2_0Tests.swift
+//  DiscGolfRB2.0Tests
+//
+//  Created by Breshears, Rob - CTC on 10/28/25.
+//
+
+import Testing
+@testable import DiscGolfRB2_0
+
+struct DiscGolfRB2_0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
